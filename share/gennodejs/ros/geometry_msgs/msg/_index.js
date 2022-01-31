ed6c938a26c9f2c8e94f13964a98607f8fd23c7a
SmallBot <casellaja@gmail.com>
2022-01-31 12:08:09 -0500

    Created

---
+++ share/gennodejs/ros/geometry_msgs/msg/_index.js
@@ -0,0 +1,64 @@
+
+"use strict";
+
+let PointStamped = require('./PointStamped.js');
+let PoseWithCovariance = require('./PoseWithCovariance.js');
+let AccelWithCovariance = require('./AccelWithCovariance.js');
+let Transform = require('./Transform.js');
+let Inertia = require('./Inertia.js');
+let PoseArray = require('./PoseArray.js');
+let Pose2D = require('./Pose2D.js');
+let PolygonStamped = require('./PolygonStamped.js');
+let Polygon = require('./Polygon.js');
+let TransformStamped = require('./TransformStamped.js');
+let TwistWithCovarianceStamped = require('./TwistWithCovarianceStamped.js');
+let AccelStamped = require('./AccelStamped.js');
+let Point = require('./Point.js');
+let Vector3Stamped = require('./Vector3Stamped.js');
+let Accel = require('./Accel.js');
+let Pose = require('./Pose.js');
+let Vector3 = require('./Vector3.js');
+let QuaternionStamped = require('./QuaternionStamped.js');
+let Wrench = require('./Wrench.js');
+let Twist = require('./Twist.js');
+let WrenchStamped = require('./WrenchStamped.js');
+let Point32 = require('./Point32.js');
+let AccelWithCovarianceStamped = require('./AccelWithCovarianceStamped.js');
+let TwistWithCovariance = require('./TwistWithCovariance.js');
+let TwistStamped = require('./TwistStamped.js');
+let InertiaStamped = require('./InertiaStamped.js');
+let Quaternion = require('./Quaternion.js');
+let PoseWithCovarianceStamped = require('./PoseWithCovarianceStamped.js');
+let PoseStamped = require('./PoseStamped.js');
+
+module.exports = {
+  PointStamped: PointStamped,
+  PoseWithCovariance: PoseWithCovariance,
+  AccelWithCovariance: AccelWithCovariance,
+  Transform: Transform,
+  Inertia: Inertia,
+  PoseArray: PoseArray,
+  Pose2D: Pose2D,
+  PolygonStamped: PolygonStamped,
+  Polygon: Polygon,
+  TransformStamped: TransformStamped,
+  TwistWithCovarianceStamped: TwistWithCovarianceStamped,
+  AccelStamped: AccelStamped,
+  Point: Point,
+  Vector3Stamped: Vector3Stamped,
+  Accel: Accel,
+  Pose: Pose,
+  Vector3: Vector3,
+  QuaternionStamped: QuaternionStamped,
+  Wrench: Wrench,
+  Twist: Twist,
+  WrenchStamped: WrenchStamped,
+  Point32: Point32,
+  AccelWithCovarianceStamped: AccelWithCovarianceStamped,
+  TwistWithCovariance: TwistWithCovariance,
+  TwistStamped: TwistStamped,
+  InertiaStamped: InertiaStamped,
+  Quaternion: Quaternion,
+  PoseWithCovarianceStamped: PoseWithCovarianceStamped,
+  PoseStamped: PoseStamped,
+};
